b741c1711f2e2a51268b56830061d05fa8300160
antonio <granadoanto123@gmail.com>
2025-11-18 20:33:37 -0600

    Initial Commit

---
+++ Vuiler+/Vuiler_App.swift
@@ -0,0 +1,17 @@
+//
+//  Vuiler_App.swift
+//  Vuiler+
+//
+//  Created by Antonio  on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Vuiler_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vuiler+Tests/Vuiler_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Vuiler_Tests.swift
+//  Vuiler+Tests
+//
+//  Created by Antonio  on 11/18/25.
+//
+
+import Testing
+@testable import Vuiler_
+
+struct Vuiler_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
